e7b47338f86baee343e6978b0904a724558e67d6
Usuario <Usuario@LAPTOP-M2PQ13GV>
2021-06-17 19:45:14 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T03_05_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 2;
+            int B = 3;
+            int C = 6;
+            int D = 8;
+            int aux = D;
+
+            Console.WriteLine("A = " + A);
+            Console.WriteLine("B = " + B);
+            Console.WriteLine("C = " + C);
+            Console.WriteLine("D = " + D);
+
+            D = B;
+            B = C;
+            C = A;
+            A = aux;
+
+            Console.WriteLine("A = " + A);
+            Console.WriteLine("B = " + B);
+            Console.WriteLine("C = " + C);
+            Console.WriteLine("D = " + D);
+        }
+    }
+}
